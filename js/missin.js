d9a561e62834072209f23cdb928c6e1c49e7757e
Gilad Meirson <113454096+GiladMeirson@users.noreply.github.com>
2024-02-19 21:41:48 -0800

    19/02

--- js/missin.js
+++ js/missin.js
@@ -1,3 +1,68 @@
+const stam =()=>{
+   return Miluim_new= [
+        {
+          "ArmyDate": "2019-12-08",
+          "M16": true,
+          "birthDay": "1999-08-30",
+          "email": "mormor2580@gmail.com",
+          "fullName": "מור מכלוף",
+          "isStudent": true,
+          "isVeg": false,
+          "job": "מתקין חלונות אלומיניום (חברת פתח חלון ודלת בע״מ)",
+          "kala": false,
+          "mag": false,
+          "matol": false,
+          "mictar": true,
+          "negev": true,
+          "personalNum": 8578249,
+          "phone": "0523338623",
+          "shoe": 44,
+          "sniper": false,
+          "tar": true,
+          "title": "מפקד כיתה",
+          "uniform": "ב",
+          class1:5,
+          'lastM':{
+            Mname:'שג',
+            MWeight:2,
+            TimeStamp:'2024-02-12T06:35',
+            Dur:3,
+        }
+        },
+        {
+          "ArmyDate": "2024-02-04",
+          "M16": false,
+          "birthDay": "2024-02-06",
+          "email": "Elielkl@hotmail.com",
+          "fullName": "אליאל קלפהולץ",
+          "isStudent": true,
+          "isVeg": false,
+          "job": "סטוד ט",
+          "kala": false,
+          "mag": false,
+          "matol": false,
+          "mictar": true,
+          "negev": false,
+          "personalNum": 7522480,
+          "phone": "0544253506",
+          "shoe": 42,
+          "sniper": false,
+          "tar": true,
+          "title": "לוחם",
+          "uniform": "ב",
+          class1:7,
+          'lastM':{
+            Mname:'סיור',
+            MWeight:3,
+            TimeStamp:'2024-02-12T16:00',
+            Dur:8,
+        }
+        }
+      ]
+}
+
+
+
 const ischeck =(value) =>{
     value?$('#showtoggleW').show():$('#showtoggleW').hide();
 }
@@ -14,12 +79,14 @@ const init = () =>{
         RenderTableM();
     })
 
-    ReadFrom("Miluim_new",(data)=>{
-        console.log('from call back' , data);
-        SOLIDERS = data;
-        data==null?SOLIDERS=[]:SOLIDERS=data;
+    //AFTER RegMobile complete.
+    // ReadFrom("Miluim_new",(data)=>{
+    //     console.log('from call back' , data);
+    //     SOLIDERS = data;
+    //     data==null?SOLIDERS=[]:SOLIDERS=data;
         
-    })
+    // })
+    SOLIDERS = stam();
 }
 
 
@@ -75,7 +142,11 @@ function calculateRestTime(soldier) {
     const lastMissionEnd = new Date(soldier.lastM.TimeStamp);
     const missionWeight = soldier.lastM.MWeight;
     const elapsedMilliseconds = now - lastMissionEnd;
-    return elapsedMilliseconds / missionWeight; // Assuming weight represents mission duration
+    let res =  elapsedMilliseconds / (missionWeight*soldier.lastM.Dur*1000*60); // Assuming weight represents mission duration
+    console.log(elapsedMilliseconds,missionWeight,soldier.lastM.Dur,res)
+    res = Math.round(res);
+    console.log(res);
+    return res;
 }
 
 
@@ -90,6 +161,8 @@ function findSoldiersWithRestTimeOrder(soldiers) {
 
     return soldiers;
 }
+
+
 
 
 
@@ -132,7 +205,7 @@ const RenderTableM=()=>{
     <td>${m.Allthetime}</td>
     <td>
     <div class="flex">
-    <button style="width:65px; font-size:16px; margin-right:5px;"  id="F-${i}" class="btn-ez-effect">מצא שיבוץ</button>   
+    <button onclick="RenderTableSOL()" style="width:65px; font-size:16px; margin-right:5px;"  id="F-${i}" class="btn-ez-effect">מצא שיבוץ</button>   
     <button style="width:65px; font-size:16px; margin-right:5px;"  id="U-${i}" class="btn-ok-effect">עריכה</button> 
     <button style="width:65px; font-size:16px; margin-right:5px;" id="X-${m.Name}" class="btn-X-effect">מחיקה</button>
     </div>
@@ -158,3 +231,98 @@ const RenderTableM=()=>{
     order: [[0, 'desc']]
   });
   }
+
+
+  const RenderTableSOL=()=>{
+    let ResArr = findSoldiersWithRestTimeOrder(SOLIDERS);
+    let str =`<table id="DB-Table2" class="display" style="width:100%">
+    <thead>
+    <tr>
+        <th>שם החייל</th>
+        <th>מחלקה</th>
+        <th>שם המשימה</th>
+        <th>משך המשימה בשעות</th>
+        <th>תאריך</th>
+        <th>משעה</th>
+        <th>עד שעה</th>
+        <th>ניקוד</th>
+        <th>פעולות</th>
+
+  
+  
+    </tr>
+  </thead>
+  <tbody>`;
+
+  for (let i = 0; i < ResArr.length; i++) {
+    const R = ResArr[i];
+    const untildate =new Date(R.lastM.TimeStamp);
+    let until = untildate.toTimeString();
+    untildate.setHours(untildate.getHours()-R.lastM.Dur);
+    let from = untildate.toTimeString();
+    let date = formatDate(untildate);
+    until = until.split(" ")[0];
+    from = from.split(" ")[0];
+    let res = calculateRestTime(R);
+
+
+    str+=`<tr>
+    <td style="font-size:22px;">${R.fullName}</td>
+    <td>${R.class1}</td>
+    <td>${R.lastM.Mname}</td>
+    <td>${R.lastM.Dur}</td>
+    <td>${date}</td>
+    <td>${from}</td>
+    <td>${until}</td>
+    <th>${res}</th>
+    <td>
+    <div class="flex">  
+    <button style="width:65px; font-size:16px; margin-right:5px;"  id="U-${i}" class="btn-ok-effect">שבץ</button> 
+    </div>
+    </td>
+  </tr>`
+  }
+
+    str+=`</tbody>`;
+    str+=`<tfoot>
+    <tr>
+    <th>שם החייל</th>
+    <th>מחלקה</th>
+    <th>שם המשימה</th>
+    <th>משך המשימה בשעות</th>
+    <th>תאריך</th>
+    <th>משעה</th>
+    <th>עד שעה</th>
+    <th>ניקוד</th>
+    <th>פעולות</th>
+  </tr>
+  </tfoot>
+  `;
+  document.getElementById('SolPH').innerHTML = str;
+  new DataTable('#DB-Table2',{
+    order: [[6, 'desc']]
+  });
+  $('#SolDBHolder').fadeIn();
+  }
+
+
+
+
+
+
+
+
+
+
+
+  function formatDate(date) {
+    const day = String(date.getDate()).padStart(2, '0'); // Get day and pad with leading zero if needed
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Get month (zero-based) and pad with leading zero if needed
+    const year = date.getFullYear(); // Get full year
+
+    return `${day}/${month}/${year}`;
+}
+
+const closeModal =(id)=>{
+    $(`#${id}`).fadeOut();
+}
